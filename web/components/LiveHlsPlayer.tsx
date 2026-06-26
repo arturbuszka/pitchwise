bb16a3d7c02fb94ea5e7035555fd7bb3a60493d4
arturbuszka <instynkt@proton.me>
2026-06-26 16:11:41 +0200

    update hls player live

--- web/components/LiveHlsPlayer.tsx
+++ web/components/LiveHlsPlayer.tsx
@@ -39,14 +39,23 @@ export function LiveHlsPlayer({
     });
 
     hlsRef.current = hls;
+    console.log("[LiveHlsPlayer] loading", hlsUrl);
     hls.loadSource(hlsUrl);
     hls.attachMedia(video);
 
-    hls.on(Hls.Events.MANIFEST_PARSED, () => {
-      video.play().catch(() => {});
+    hls.on(Hls.Events.MANIFEST_PARSED, (_e, data) => {
+      console.log("[LiveHlsPlayer] manifest parsed, levels:", data.levels?.length);
+      video.play().then(() => console.log("[LiveHlsPlayer] playing")).catch((err) => console.warn("[LiveHlsPlayer] play() rejected:", err));
+    });
+    hls.on(Hls.Events.LEVEL_LOADED, (_e, data) => {
+      console.log("[LiveHlsPlayer] level loaded, fragments:", data.details?.fragments?.length, "live:", data.details?.live);
+    });
+    hls.on(Hls.Events.FRAG_BUFFERED, (_e, data) => {
+      console.log("[LiveHlsPlayer] frag buffered:", data.frag?.sn);
     });
 
     hls.on(Hls.Events.ERROR, (_evt, data) => {
+      console.error("[LiveHlsPlayer] HLS error:", data.type, data.details, "fatal:", data.fatal, data.response?.code ?? "");
       if (data.fatal) {
         // For live streams, try to recover instead of giving up
         if (data.type === Hls.ErrorTypes.NETWORK_ERROR) {
